39f86f8327627b22f3f4f33af4234356ee17b154
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-26 09:04:53 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,12 +28,6 @@ def monopoly_with_third_tier
 	}	
 end
 
-epic_tragedy = {
-  :montague => {
-    :patriarch => {  
-      :name => "Lord Montague",
-      :age => "53"
-  },
 
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
