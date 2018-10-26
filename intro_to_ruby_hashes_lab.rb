2d2e1e9347e2b629573c3b409c5e4ba1b5d18e0d
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-26 09:36:43 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,7 +51,9 @@ monopoly = {:railroads => {
 	    :pennsylvania_railroad => {
 	      "mortgage_value"=>"$200"
 	    },
-	    :b_and_o_railroad => {},
+	    :b_and_o_railroad => {
+	      "mortgage_value"=>"$400"
+	    },
 	    :shortline => {}
 	  },
 	  :rent_in_dollars => {
